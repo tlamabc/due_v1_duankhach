8719db583bb03da1ac359f7f981501c7878f588f
nhacnt17 <nhacntpd06031@fpt.edu.vn>
2024-05-19 09:51:25 +0700

    push NVLogFormWidget 146

--- lib/pages/n_v_user/n_v_log/n_v_log_form/n_v_log_form_widget.dart
+++ lib/pages/n_v_user/n_v_log/n_v_log_form/n_v_log_form_widget.dart
@@ -138,16 +138,218 @@ class _NVLogFormWidgetState extends State<NVLogFormWidget> {
                                       mainAxisAlignment:
                                           MainAxisAlignment.center,
                                       children: [
-                                        // Expanded(
-                                        //   child: wrapWithModel(
-                                        //     model: _model.showMissionShortModel,
-                                        //     updateCallback: () =>
-                                        //         setState(() {}),
-                                        //     child: ShowMissionShortWidget(
-                                        //        suCo: suCo
-                                        //     ),
-                                        //   ),
-                                        // ),
+                                        Expanded(
+                                          child: wrapWithModel(
+                                            model: _model.showMissionShortModel,
+                                            updateCallback: () =>
+                                                setState(() {}),
+                                            child: Column(
+                                              mainAxisSize: MainAxisSize.max,
+                                              crossAxisAlignment: CrossAxisAlignment.center,
+                                              children: [
+                                                Align(
+                                                  alignment: AlignmentDirectional(0.0, 0.0),
+                                                  child: Text(
+                                                    'Thông tin nhiệm vụ',
+                                                    textAlign: TextAlign.center,
+                                                    style: FlutterFlowTheme.of(context).bodyMedium.override(
+                                                      fontFamily: 'Roboto',
+                                                      fontSize: 16.0,
+                                                      letterSpacing: 0.0,
+                                                      fontWeight: FontWeight.bold,
+                                                    ),
+                                                  ),
+                                                ),
+                                                Container(
+                                                  width: double.infinity,
+                                                  height: 45.0,
+                                                  decoration: BoxDecoration(
+                                                    color: FlutterFlowTheme.of(context).secondaryBackground,
+                                                    borderRadius: BorderRadius.circular(15.0),
+                                                  ),
+                                                  child: Padding(
+                                                    padding: EdgeInsets.all(5.0),
+                                                    child: Row(
+                                                      mainAxisSize: MainAxisSize.max,
+                                                      mainAxisAlignment: MainAxisAlignment.start,
+                                                      children: [
+                                                        Container(
+                                                          width: 70.0,
+                                                          height: double.infinity,
+                                                          decoration: BoxDecoration(
+                                                            color: FlutterFlowTheme.of(context).secondaryBackground,
+                                                          ),
+                                                          child: Align(
+                                                            alignment: AlignmentDirectional(-1.0, 0.0),
+                                                            child: Text(
+                                                              'Sự cố',
+                                                              style: FlutterFlowTheme.of(context).bodyMedium.override(
+                                                                fontFamily: 'Readex Pro',
+                                                                letterSpacing: 0.0,
+                                                              ),
+                                                            ),
+                                                          ),
+                                                        ),
+                                                        VerticalDivider(
+                                                          thickness: 2.0,
+                                                          color: FlutterFlowTheme.of(context).primary,
+                                                        ),
+                                                        Text(
+                                                          ' ${widget.suCo.loaiSuCo}',
+                                                          style: FlutterFlowTheme.of(context).bodyMedium.override(
+                                                            fontFamily: 'Readex Pro',
+                                                            letterSpacing: 0.0,
+                                                          ),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                  ),
+                                                ),
+                                                Container(
+                                                  width: double.infinity,
+                                                  height: 45.0,
+                                                  decoration: BoxDecoration(
+                                                    color: FlutterFlowTheme.of(context).secondaryBackground,
+                                                    borderRadius: BorderRadius.circular(15.0),
+                                                  ),
+                                                  child: Padding(
+                                                    padding: EdgeInsets.all(5.0),
+                                                    child: Row(
+                                                      mainAxisSize: MainAxisSize.max,
+                                                      mainAxisAlignment: MainAxisAlignment.start,
+                                                      children: [
+                                                        Container(
+                                                          width: 70.0,
+                                                          height: double.infinity,
+                                                          decoration: BoxDecoration(
+                                                            color: FlutterFlowTheme.of(context).secondaryBackground,
+                                                          ),
+                                                          child: Align(
+                                                            alignment: AlignmentDirectional(-1.0, 0.0),
+                                                            child: Text(
+                                                              'Vị trí',
+                                                              style: FlutterFlowTheme.of(context).bodyMedium.override(
+                                                                fontFamily: 'Readex Pro',
+                                                                letterSpacing: 0.0,
+                                                              ),
+                                                            ),
+                                                          ),
+                                                        ),
+                                                        VerticalDivider(
+                                                          thickness: 2.0,
+                                                          color: FlutterFlowTheme.of(context).primary,
+                                                        ),
+                                                        Text(
+                                                          ' ${widget.suCo.vitriP}',
+                                                          style: FlutterFlowTheme.of(context).bodyMedium.override(
+                                                            fontFamily: 'Readex Pro',
+                                                            letterSpacing: 0.0,
+                                                          ),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                  ),
+                                                ),
+                                                Container(
+                                                  width: double.infinity,
+                                                  height: 90.0,
+                                                  decoration: BoxDecoration(
+                                                    color: FlutterFlowTheme.of(context).secondaryBackground,
+                                                    borderRadius: BorderRadius.circular(15.0),
+                                                  ),
+                                                  child: Padding(
+                                                    padding: EdgeInsets.all(5.0),
+                                                    child: Row(
+                                                      mainAxisSize: MainAxisSize.max,
+                                                      mainAxisAlignment: MainAxisAlignment.start,
+                                                      children: [
+                                                        Container(
+                                                          width: 70.0,
+                                                          height: double.infinity,
+                                                          decoration: BoxDecoration(
+                                                            color: FlutterFlowTheme.of(context).secondaryBackground,
+                                                          ),
+                                                          child: Align(
+                                                            alignment: AlignmentDirectional(-1.0, 0.0),
+                                                            child: Text(
+                                                              'Mô tả',
+                                                              textAlign: TextAlign.start,
+                                                              style: FlutterFlowTheme.of(context).bodyMedium.override(
+                                                                fontFamily: 'Readex Pro',
+                                                                letterSpacing: 0.0,
+                                                              ),
+                                                            ),
+                                                          ),
+                                                        ),
+                                                        VerticalDivider(
+                                                          thickness: 2.0,
+                                                          color: FlutterFlowTheme.of(context).primary,
+                                                        ),
+                                                        Expanded(
+                                                          child: Text(
+                                                            ' ${widget.suCo.chiTiet}',
+                                                            textAlign: TextAlign.start,
+                                                            maxLines: 10,
+                                                            style: FlutterFlowTheme.of(context).bodyMedium.override(
+                                                              fontFamily: 'Readex Pro',
+                                                              letterSpacing: 0.0,
+                                                              lineHeight: 1.0,
+                                                            ),
+                                                          ),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                  ),
+                                                ),
+                                                Container(
+                                                  width: double.infinity,
+                                                  height: 45.0,
+                                                  decoration: BoxDecoration(
+                                                    color: FlutterFlowTheme.of(context).secondaryBackground,
+                                                    borderRadius: BorderRadius.circular(15.0),
+                                                  ),
+                                                  child: Padding(
+                                                    padding: EdgeInsets.all(5.0),
+                                                    child: Row(
+                                                      mainAxisSize: MainAxisSize.max,
+                                                      mainAxisAlignment: MainAxisAlignment.start,
+                                                      children: [
+                                                        Container(
+                                                          width: 70.0,
+                                                          height: 100.0,
+                                                          decoration: BoxDecoration(
+                                                            color: FlutterFlowTheme.of(context).secondaryBackground,
+                                                          ),
+                                                          child: Align(
+                                                            alignment: AlignmentDirectional(-1.0, 0.0),
+                                                            child: Text(
+                                                              'Tình trạng',
+                                                              style: FlutterFlowTheme.of(context).bodyMedium.override(
+                                                                fontFamily: 'Readex Pro',
+                                                                letterSpacing: 0.0,
+                                                              ),
+                                                            ),
+                                                          ),
+                                                        ),
+                                                        VerticalDivider(
+                                                          thickness: 2.0,
+                                                          color: FlutterFlowTheme.of(context).primary,
+                                                        ),
+                                                        Text(
+                                                          widget.suCo.khanCap ? 'Khẩn cấp' : 'Bình thường',
+                                                          style: FlutterFlowTheme.of(context).bodyMedium.override(
+                                                            fontFamily: 'Readex Pro',
+                                                            letterSpacing: 0.0,
+                                                          ),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                  ),
+                                                ),
+                                              ].divide(SizedBox(height: 8.0)),
+                                            ),
+                                          ),
+                                        ),
                                       ],
                                     ),
                                     Padding(
